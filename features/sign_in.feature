eedcae18ccebe74233404f0497e632ed3513b575
GennadyCC <gennady.cc@gmail.com>
2018-06-23 11:33:56 -0700

    init

--- features/sign_in.feature
+++ features/sign_in.feature
@@ -1,5 +1,6 @@
 Feature: User can LogIn to app
 
+  @login
   Scenario Outline: I can LogIn if I provide valid Username and Password
     Given I go to LogIn screen
     When I tap the SignUp/LogIn button
@@ -7,7 +8,21 @@ Feature: User can LogIn to app
     Then I provide "<email>" and "<password>"
     And I tap LogIn button
     Then I can see "<username>" on top of the screen
-#
+    And I close application
+
     Examples:
     |email                |password   |username    |
     |r2xxxxd2+3@gmail.com |password.3 |r2xxxxd2.3  |
+
+  @signup
+  Scenario Outline: I can Sign up if I provide valid data
+    Given I go to LogIn screen
+    When I tap the SignUp/LogIn button
+    And I tap Sign up link
+    Then I I provide "<email>", "<password>" and "<username>"
+    And I tap the Sign up button
+    Then I can see "<username>" on top of the screen
+
+  Examples:
+  |email                |password   |username    |
+  |r2xxxxd2+4@gmail.com |password.4 |r2xxxxd2.4  |
--- features/step_definitions/sign_in_steps.rb
+++ features/step_definitions/sign_in_steps.rb
@@ -1,33 +1,39 @@
 Given(/^I go to LogIn screen$/) do
-  puts("I tap User icon")
   find_element(xpath: "//android.support.v7.app.ActionBar.Tab[@index = '4']").click
 end
 
 When(/^I tap the SignUp\/LogIn button$/) do
-  puts("I tap SignUp button")
   find_element(id: "action_social").click
 end
 
 Then(/^I tap LogIn link$/) do
-  puts("I tap LogIn link")
   find_element(id: "action_login").click
 end
 
 Then(/^I provide "([^"]*)" and "([^"]*)"$/) do |email, pass|
-  puts("I provide #{email} and #{pass}")
   find_element(id: "field_mail").send_keys(email)
   find_element(id: "field_password").send_keys(pass)
   @driver.hide_keyboard
 end
 
 And(/^I tap LogIn button$/) do
-  puts("I tap LogIn button")
   find_element(xpath: "//android.widget.Button[@index = '4']").click
   sleep 5
 end
 
 Then(/^I can see "([^"]*)" on top of the screen$/) do |username|
-  puts("I can see #{username} on the screen")
-  sleep 15
+  make_screenshot("login")
+  sleep 1
+end
 
+And(/^I tap Sign up link$/) do
+  pending
+end
+
+Then(/^I I provide "([^"]*)", "([^"]*)" and "([^"]*)"$/) do |email, password, username|
+  pending
+end
+
+And(/^I tap the Sign up button$/) do
+  pending
 end
